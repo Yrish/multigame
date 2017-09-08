c9ccfe4b72c02a5149a52120973d6fe66f6ed24c
Yrish <Yrish3@gmail.com>
2017-09-08 16:30:04 -0600

    Initial commit

---
+++ webSupport.py
@@ -0,0 +1,55 @@
+import requests
+import json
+
+class webSupport:
+
+    def startSession():
+        webSupport.s = requests.Session()
+
+    def get(url):
+        return webSupport.s.get(url).text
+
+    def update(url, json):
+        webSupport.s.get(url, params={'update':json})
+
+    def __import__():
+        webSupport.startSession()
+
+    def buildData(**kwords):
+        data = {"entities":kwords.get("entities", {}), "world":kwords.get("world", {})}
+        return {"method":kwords.get("method", "None"),"data":json.dumps(data)}
+
+    def buildRequests(lis = {}, *reqs):
+        ret = []
+        for item in reqs:
+            ret.append(item)
+        lis["requests"] = ret
+        return lis
+
+    def buildRequest(typ, **get):
+        req = {"request_type":typ}
+        for key in get:
+            req[key] = get[key]
+        return req
+
+url = 'https://python-brysen.c9users.io/'
+webSupport.startSession()
+r = webSupport.get(url)
+
+#Unload data
+rec = json.loads(r)
+rec["pos_x"] += 50
+
+'''
+print(r, end = "\n-----------------------\n")
+#req = requests.post(url, data={"method":"UPDATE","data":json.dumps(rec)})
+rec["pos_y"] += 50
+print(webSupport.buildData(method="UPDATE", entities=rec))
+req = requests.post(url, data=webSupport.buildData(method="UPDATE", entities=rec))
+print(req.text)
+'''
+
+req = webSupport.buildRequests({}, webSupport.buildRequest("world", world_name="tester"))
+print(req, end="\n*****************")
+
+print(requests.post(url, data={"method":"REQUEST","data":json.dumps(req)}).text)
