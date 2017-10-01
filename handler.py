c1125ebcb2285731022a32271327f675aecd0750
Brsayen Goodwin <Braysen@user-names.local>
2017-10-01 10:17:37 -0600

    Started programming launcher and creating file layout

---
+++ handler.py
@@ -0,0 +1,3 @@
+class Handler:
+
+    serverURL = None
--- launcher.py
+++ launcher.py
@@ -1,4 +1,11 @@
+from handler import Handler
+
 class Launcher:
 
     def __init__(this, url):
+        Handler.serverURL = url
+
+
+launch = Launcher("Hello World")
+print(Handler.serverURL)
         
